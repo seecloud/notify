c8c97ec37f26195a007443d108dbf210cf581561
Max Mazur <mmaxur@mirantis.com>
2016-12-06 13:44:09 +0200

    fix package installation

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name="notify",
       version="0.1",
@@ -8,7 +9,7 @@ setup(name="notify",
       url="https://github.com/seecloud/notify",
       author="<name>",
       author_email="<name>@mirantis.com",
-      packages=["notify"],
+      packages=find_packages(),
       entry_points={
           "console_scripts": [
               "notify-api = notify.main:main"
