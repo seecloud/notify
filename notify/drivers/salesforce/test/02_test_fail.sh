a1928204d6fa1ef7ab598b0f721135300078996d
sirmax123 <sirmax123@gmail.com>
2016-12-17 04:19:04 +0200

    SFDC API v 0.1 (#5)

---
+++ notify/drivers/salesforce/test/02_test_fail.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+NOTIFY_API_URL=${NOTIFY_API_URL-"http://127.0.0.1:5000/api/v1/notify"}
+
+curl -v  ${NOTIFY_API_URL}/fail -XPOST -d '
+{
+    "description":   {
+    "affected_hosts": [
+            "host=ic3-ctl01-scc1",
+            "host=ic3-ctl01-scc2",
+            "host=ic3-ctl01-scc"
+    ],
+    "description": "Test     The CPU usage is too high (controller node).             (CRITICAL, rule=avg(cpu_idle)=5, current=1.65, host=ic3-ctl01-scc)                     No datapoint have been received over the last 60 seconds                     (UNKNOWN, rule=min(fs_space_percent_free[fs=/var/log])2, \n",
+    "long_date_time": "Fri Dec 2 14:14:53 UTC 2016",
+    "service": "glance1"
+    },
+    "severity":    "INFO",
+    "who":         "00-global-clusters-SIT12",
+    "what":        "glance1"
+}
+'
